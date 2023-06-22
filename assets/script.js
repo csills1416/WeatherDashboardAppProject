556177708fc638331d8ccc50f0812a0b7ca2519e
CSillus <71670415+csills1416@users.noreply.github.com>
2023-06-22 15:17:35 -0400

    added a function that gets the geolocation. Need Geolocation api

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,24 @@ cityForm.addEventListener('submit', function(event) {
     }
 });
 
+function getCoordinates(city) {
+    const geocodingApiKey = ''; 
+    const geocodingUrl = `https://api.geocoding-service.com/geocode?address=${encodeURIComponent(city)}&key=${geocodingApiKey}`;
+  
+    // Make a request to the geocoding service API to obtain the coordinates for the city
+    fetch(geocodingUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'OK' && data.results.length > 0) {
+          const latitude = data.results[0].geometry.lat;
+          const longitude = data.results[0].geometry.lng;
+          getWeatherData(latitude, longitude);
+          saveToSearchHistory(city);
+        } else {
+          console.log('Error: Unable to retrieve coordinates for the city.');
+        }
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+    }
