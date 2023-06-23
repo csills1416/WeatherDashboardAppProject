0fff49e68f7d2e164d7aa4dcde8aacbff08157cb
CSillus <71670415+csills1416@users.noreply.github.com>
2023-06-23 09:16:20 -0400

    got the appropriate api key

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,71 @@
-const apiKey = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+const apiKey = '11af795699dc87d9506f101730b7ae93EY'; // Replace with your actual OpenWeatherMap API key
 const cityForm = document.getElementById('city');
 const cityInput = document.getElementById('city-input');
 const currentWeather = document.getElementById('current-weather');
 const forecastInfo = document.getElementById('forecast-info');
 
 cityForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const city = cityInput.ariaValueMax.trim();
-    if (city !== '') {
-        GeolocationCoordinates(city);
-        cityInput.value - '';
-    }
+  event.preventDefault();
+  const city = cityInput.value.trim();
+  if (city !== '') {
+    getCoordinates(city);
+    cityInput.value = '';
+  }
 });
 
 function getCoordinates(city) {
-    const geocodingApiKey = 'api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}'; 
-    const geocodingUrl = `https://api.geocoding-service.com/geocode?address=${encodeURIComponent(city)}&key=${geocodingApiKey}`;
-  
-    // Make a request to the geocoding service API to obtain the coordinates for the city
-    fetch(geocodingUrl)
-      .then(response => response.json())
-      .then(data => {
-        if (data.status === 'OK' && data.results.length > 0) {
-          const latitude = data.results[0].geometry.lat;
-          const longitude = data.results[0].geometry.lng;
-          getWeatherData(latitude, longitude);
-          saveToSearchHistory(city);
-        } else {
-          console.log('Error: Unable to retrieve coordinates for the city.');
-        }
-      })
-      .catch(error => {
-        console.log('Error:', error);
-      });
-    }
+  const geocodingApiKey = '11af795699dc87d9506f101730b7ae93'; // Replace with your actual geocoding API key
+  const geocodingUrl = `https://api.geocoding-service.com/geocode?address=${encodeURIComponent(city)}&key=${11af795699dc87d9506f101730b7ae93}`;
 
-    function getWeatherData(latitude, longitude) {
-        const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-      // Couldn't seem to find a way to get the weather data from the API provided. So I looked up this api on the openweathermap.org website.
-        fetch(weatherApiUrl)
-          .then(response => response.json())
-          .then(data => {
-            updateCurrentWeather(data);
-            updateForecast(data);
-          })
-          .catch(error => {
-            console.log('Error:', error);
-          });
+  // Make a request to the geocoding service API to obtain the coordinates for the city
+  fetch(geocodingUrl)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'OK' && data.results.length > 0) {
+        const latitude = data.results[0].geometry.lat;
+        const longitude = data.results[0].geometry.lng;
+        getWeatherData(latitude, longitude);
+        saveToSearchHistory(city);
+      } else {
+        console.log('Error: Unable to retrieve coordinates for the city.');
       }
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+}
 
-    function updateCurrentWeather(weatherData) {
-    }
+function getWeatherData(latitude, longitude) {
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${11af795699dc87d9506f101730b7ae93}`;
 
-    function updateForecastInfo(weatherData) {
-    }
+  fetch(weatherApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      updateCurrentWeather(data);
+      updateForecastInfo(data);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+}
 
-    function saveToSearchHistory(city) {
-    }
+function updateCurrentWeather(weatherData) {
 
-    function displaySearchHistory() {
-        const searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
-        if (searchHistory === null) {
-            searchHistory = [];
-        } else {
-            searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
-        }
-        searchHistory.push(city);
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-    }
-    
-    displaySearchHistory();
+}
+
+function updateForecastInfo(weatherData) {
+
+}
+
+function saveToSearchHistory(city) {
+  let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+  if (searchHistory === null) {
+    searchHistory = [];
+  }
+  searchHistory.push(city);
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+}
+
+function displaySearchHistory() {
+ 
+}
